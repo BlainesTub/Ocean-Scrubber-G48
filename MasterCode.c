aff532d5f632f95b5b42727706066a5cc939a94b
Blaine Tubungbanua <btubungbanua@uvic.ca>
2020-03-13 11:39:20 -0700

    Compiled First Master Draft
    Pulled Lab 2 Code,
    Reconfigured for 2 motors

--- MasterCode.c
+++ MasterCode.c
@@ -42,7 +42,57 @@ void monitorInput()
 	}
 }
 
+<<<<<<< Updated upstream
 void RobotOperation()
+=======
+int monitorLight()
+{
+	// Static variables are a special class of variables that maintain
+	// their values between subsequent calls to a function.  The intialization
+	// values are only stored in the variables when the function is first called.
+	// After that, the values that were in the variable at the end of the last time
+	// the function is called will be stored in the variable when the function
+	// execution is started.
+	static int minLevelIR1 = 4096;	// Minimum light level seen by IR sensor 1
+	static int maxLevelIR1 = 0;			// Maximum light level seen by IR sensor 1
+	static int diffLevelIR1 = 0;		// Delta between maximum and minimum seen in last 0.1 seconds
+
+	int lightLevel1 = SensorValue[IRsensor1];
+
+	// Check if 100 msecs have elapsed.
+	if ( time1[T1] > 100 )  {
+
+		// 100 msecs have elapsed.  Compute delta of light level.
+		diffLevelIR1 = maxLevelIR1 - minLevelIR1;
+
+		// Reset calculation for next 100 msecs.
+		maxLevelIR1 = 0;
+		minLevelIR1 = 4096;
+		clearTimer(T1);
+
+		} else {
+
+		// Check for new minimum/maximum light levels.
+		if ( lightLevel1 < minLevelIR1 ) {
+			minLevelIR1 = lightLevel1;
+			} else if ( lightLevel1 > maxLevelIR1 ) {
+			maxLevelIR1 = lightLevel1;
+		}
+	}
+
+	return(diffLevelIR1);
+}
+
+void CollectSensorData()
+{
+
+	writeDebugStream("Sonar: %d\n", SensorValue[sonar]);
+
+
+}
+
+void RobotDrive()
+>>>>>>> Stashed changes
 {
 	enum T_test_state{          //Initialize 3 states
 		stopped = 0,
